77cdb4929bc40582643f2c101481e573628273d7
TIEMBOLOHO FANNY <fannytiem2@yahoo.fr>
2023-02-01 22:24:16 -0500

    clean code

--- src/main/java/com/tfa/service/APIClient.java
+++ src/main/java/com/tfa/service/APIClient.java
@@ -6,11 +6,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.tfa.dto.DepartementDto;
 
+/**
+ * Interface permettant de communiquer avec l'API departement via spring cloud
+ * @author fanny
+ *
+ */
 //@FeignClient(url = "http://localhost:8081",value = "DEPARTEMENT-SERVICE")
 @FeignClient(name = "SERVICE-DEPARTEMENT")
 public interface APIClient {
 
 	@GetMapping("api/departements/code/{code}")
 	DepartementDto obtenirDepartement(@PathVariable("code") String code) ;
-
 }
